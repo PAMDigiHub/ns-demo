3b981187448a4b45d82be80884939ea80ef920c9
YolaineCourteau <ycourteau@collegeshawinigan.qc.ca>
2020-04-18 10:00:30 -0400

    for ben

--- src/app/rtdb.service.ts
+++ src/app/rtdb.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { Injectable, NgZone } from '@angular/core';
+import { AngularFireDatabase, AngularFireAction, AngularFireList } from '@angular/fire/database';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Call } from '@src/app/call.model';
 
 
@@ -10,66 +10,58 @@ import { Call } from '@src/app/call.model';
 })
 export class RtdbService {
 
+  //items: Observable<any[]>; // 8.4
+
+
+  // private callsCollection: Call[] = [];
+  // private callSubject$: BehaviorSubject<Call[]>;
+
+  items$: Observable<AngularFireAction<firebase.database.DataSnapshot>[]>;
+  call$: BehaviorSubject<string | null>;
+  items: Observable<any>;
   private callsCollection: Call[] = [];
   private callSubject$: BehaviorSubject<Call[]>;
 
+  itemsRef: AngularFireList<any>;
+
   constructor(
-    private firestore: AngularFireDatabase,
+    private database: AngularFireDatabase,
     private ngZone: NgZone
   ) {
-    /** Le BehaviorSubject sera utilisé pour émettre les Calls qui sont mis à jour par Firebase
-     *  le BehaviorSubject nous offre la possibilité d'émettre la liste immédiatement lors d'un Subscribe
-     */
-    this.callSubject$ = new BehaviorSubject(this.callsCollection);
 
-    firebase.init({
-      // Optionally pass in properties for database, authentication and cloud messaging,
-      // see their respective docs.
-    }).then(
-      () => {
-        console.log('firebase.init done');
-      },
-      error => {
-        console.log(`firebase.init error: ${error}`);
-      }
+
+    //this.call$ = new BehaviorSubject(null);
+
+    //this.callSubject$ = new BehaviorSubject(this.callsCollection);
+
+    //this.items = database.list('ns-demo-4dd52').snapshotChanges();
+    //console.log(this.items);
+
+    this.itemsRef = database.list('ns-demo-4dd52');
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes => {
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+        console.log("SERVICE constructor this.items = ");
+        console.log(this.items);
+      })
     );
 
-    /** Cette fonction est appelée chaque fois qu'il y a un changement dans la liste de Call de Firebase */
-    const onChildEvent = (change) => {
-      const newCalls = [];
-      Object.keys(change.value).forEach((key, index) => {
-        const call: Call = Object.assign({}, { id: key, ...change.value[key] });
-        newCalls.push(call);
-      },
-      );
-      this.callsCollection = newCalls;
-
-      /**
-       * La librairie Firebase que nous utilisons n'est pas une librairie Angular. Elle s'éxécute donc hors de la Zone utilisée par Angular.
-       * Le mécanisme de détection des changements d'Angular n'est donc pas notifié du changement que nous voulons déclancher lorsque nous
-       * émettons une nouvelle liste de Call. Nous devons forcer la détection des changements.
-       *
-       * Plus d'informations : https://angular.io/guide/zone
-       */
-      this.ngZone.run(args => {
-        this.callSubject$.next(newCalls);
-      });
-    };
-
-    /**
-     * Ajoute un listener dans Firebase qui nous permet d'écouter les changements dans la liste de Call.
-     * La méthode onChildEvent sera exécutée avec comme entrée un objet représentant le changement.
-     */
-    firebase.addValueEventListener(onChildEvent, 'calls');
   }
 
 
-  getCalls(): Observable<Call[]> {
-    return this.callSubject$.asObservable();
+  getCalls(): Observable<any> {
+    //console.log('SERVICE getCalls this.items$ = ');
+
+    //this.items$ = this.database.list('ns-demo-4dd52').snapshotChanges();
+    //console.log(this.items);
+    //return this.items;
+    return this.database.list('ns-demo-4dd52').snapshotChanges();
   }
 
   getCall(id: string): Observable<Call> {
-    return this.callSubject$.asObservable().pipe(map(calls => calls.filter(value => value.id === id)[0]));
+    //return this.callSubject$.asObservable().pipe(map(calls => calls.filter(value => value.id === id)[0]));
+    return;
   }
 
 
@@ -80,14 +72,14 @@ export class RtdbService {
 
   updateCall(call: Call) {
     delete call.id;
-    this.firestore.database.ref('calls/' + call.id).update(call);
+    //this.firestore.database.ref('calls/' + call.id).update(call);
   }
 
   deleteCall(callId: string) {
     // this.firestore.doc('calls/' + callId).delete();
     console.log('deleteCall callId = ');
     console.log(callId);
-    this.firestore.list('calls').remove(callId);
+    //this.firestore.list('calls').remove(callId);
   }
 
 }
--- src/app/rtdb/rtdb.component.ts
+++ src/app/rtdb/rtdb.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { RtdbService } from '../rtdb.service';
+import { Call } from '../call.model';
 
 @Component({
   selector: 'app-rtdb',
@@ -10,21 +11,41 @@ import { RtdbService } from '../rtdb.service';
 })
 export class RtdbComponent implements OnInit {
 
-  items: Observable<any[]>;
+  //items: Observable<any[]>; // 8.4
 
+  calls: Call[] = [];
+
+  //calls: Observable<Call[]>;
   constructor(
-    database: AngularFireDatabase,
-    rtdbservice: RtdbService
+    // database: AngularFireDatabase, // 8.4
+    private rtdbservice: RtdbService
   ) {
-    this.items = database.list('ns-demo-4dd52').valueChanges();
-    console.log(this.items);
+    // 8,4
+    // this.items = database.list('ns-demo-4dd52').valueChanges();
+    // console.log(this.items);
   }
 
   ngOnInit() {
+    /** Il faut subscribe au service pour recevoir les donnees, et les modifications */
+    // this.rtdbservice.getCalls().subscribe((calls) => {
+    //   this.calls = calls;
+    //   console.log("FIREBASE onInit calls = ");
+    //   console.log(this.calls);
+    // });
+
+    console.log("onInit");
+  }
+
+  ngAfterViewInit() {
+    this.rtdbservice.getCalls().subscribe((calls) => {
+      this.calls = calls;
+      console.log("FIREBASE onInit calls = ");
+      console.log(this.calls);
+    });
   }
 
   // d/termine si ligne paire ou impaire
-  templateSelector(item, index: number, items: any) {
-    return index % 2 === 0 ? "red" : "green";
-  }
+  // templateSelector(item, index: number, items: any) {
+  //   return index % 2 === 0 ? "red" : "green";
+  // }
 }
